991c49603fcf74e5e678384bbd6c5daf228034dc
Melissa <melissasouza.dev@gmail.com>
2024-05-03 17:41:06 -0300

    feat: added code style configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const server = express(); //instancia do servidor
 
+interface Teste {
+
+}
+
 server.get('/', (req, res) => {
     return res.send('Olá, dev!');
 });
