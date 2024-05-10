e9f4a3ba67580d770aebd083295fddf19cf52774
Melissa <melissasouza.dev@gmail.com>
2024-05-10 17:24:06 -0300

    feat: added yup translations and aborty early on validate method

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -8,20 +8,24 @@ interface ICidade {
 
 const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body)
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
-    return res.json({
-      errors: {
-        default: yupError.message,
-      }
+    validatedData = await bodyValidation.validate(req.body, {abortEarly: false})
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      errors[error.path] = error.message;
     });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
   }
 
   console.log(validatedData);
